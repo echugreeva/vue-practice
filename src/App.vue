a4c5932027a580df96bf6201a83d861aa84aafcb
echugreeva <e.chugreeva@gmail.com>
2023-02-15 19:21:55 +0200

    half of the project w/o input

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+    <the-header title="Learn Vue with..."></the-header>
+    <stored-res :resources="storedResources"></stored-res>
+</template>
+
+<script>
+// import ResourceItem from './components/learning-resources/ResourseItem.vue'
+import TheHeader from './components/UI/TheHeader.vue'
+import StoredRes from './components/learning-resources/StoredRes.vue'
+
+export default {
+        components: {
+            // 'resource-item': ResourceItem,
+            'stored-res': StoredRes,
+            'the-header': TheHeader,
+            
+        },
+        data () {
+            return {
+                storedResources: [
+                    {
+                        id:'official-guide',
+                        title: 'official guide',
+                        description: 'adf',
+                        link: 'fv'
+                    },
+                    {
+                        id:'google',
+                        title: 'google',
+                        description: 'adf',
+                        link: 'fv'
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<style>
+    * {
+        box-sizing: border-box;
+    }
+    html {
+        font-family: 'Roboto', sans-serif;
+    }
+
+    body {
+        margin: 0
+    }
+</style>
---
+++ src/components/learning-resources/StoredRes.vue
@@ -0,0 +1,19 @@
+<template>
+    <ul>
+        <resource-item v-for="res in resources" 
+        :key="res.id" 
+        :title ="res.title"
+        :description="res.description"
+        :link="res.link" ></resource-item>
+    </ul>
+</template>
+
+<script>
+import ResourceItem from './ResourseItem.vue'
+export default {
+  props: ['resources'],
+  components: {
+    'resource-item': ResourceItem,
+  }
+};
+</script>
